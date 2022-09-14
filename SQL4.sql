f2b3e3e5d918245634014bda24b06164998ccdc7
OsmanFurkanNalbant <osmanfnalbant@outlook.com>
2022-09-14 15:52:52 +0300

    yeniden

--- SQL4.sql
+++ SQL4.sql
@@ -6,3 +6,9 @@ SELECT DISTINCT count(replacement_cost) from film;
 
 --3)
 SELECT COUNT(title) from film where title like 'T%' and rating = 'G';
+
+--4)
+SELECT country from country where count(country)=5;
+
+--5)
+select count(country) from country where country like '%R' or country like 'r%';
